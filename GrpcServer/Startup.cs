19e9b1ade8eb1b7d173ae339f307939260a39e45
mvslegt <mvslegt@gmail.com>
2021-09-21 18:54:04 +0200

    Cleaned usings

--- GrpcServer/Startup.cs
+++ GrpcServer/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using FormulaOne;
 
 namespace GrpcServer
 {
